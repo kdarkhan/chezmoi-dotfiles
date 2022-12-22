40df50528b3e31eb366bcd0f319900dd2c02d2aa
Darkhan Kubigenov <darkhanu@gmail.com>
2022-12-22 22:47:31 +0000

    Disable cspell

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -458,7 +458,7 @@ function MyLspConfig(opts)
   local null_ls = require('null-ls')
   null_ls.setup({
     sources = {
-      null_ls.builtins.diagnostics.cspell,
+      -- null_ls.builtins.diagnostics.cspell,
       null_ls.builtins.formatting.stylua,
     },
     debug = false,
