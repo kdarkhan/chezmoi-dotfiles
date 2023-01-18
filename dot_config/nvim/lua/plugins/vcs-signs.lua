7e03140b13f0b4bfc932bbf1ad99d7f077b77d45
Darkhan Kubigenov <darkhanu@gmail.com>
2023-01-18 11:33:23 +0000

    Add vim-signify to handle mercurial repos

--- dot_config/nvim/lua/plugins/vcs-signs.lua
+++ dot_config/nvim/lua/plugins/vcs-signs.lua
@@ -1,7 +1,26 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('gitsigns').setup()
-  end,
+  {
+    'lewis6991/gitsigns.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup()
+    end,
+  },
+  {
+    'mhinz/vim-signify',
+    config = function()
+      vim.g.signify_skip = { vcs = { allow = { 'hg' } } }
+      -- replicate gitsigns.nvim highlighting
+      vim.g.signify_sign_add = '┃' -- default '+'
+      vim.g.signify_sign_delete = '▁'  -- default = '_'
+      vim.g.signify_sign_delete_first_line = '▔' -- default '‾'
+      vim.g.signify_sign_change = '┃' -- default '!'
+      -- vim.g.signify_sign_change_delete     = vim.g.signify_sign_change . vim.g.signify_sign_delete_first_line
+      vim.api.nvim_set_hl(0, 'SignifySignAdd', { link = 'diffAdded' })
+      vim.api.nvim_set_hl(0, 'SignifySignChange', { link = 'diffChanged' })
+      vim.api.nvim_set_hl(0, 'SignifySignChangeDelete', { link = 'SignifySingChange' })
+      vim.api.nvim_set_hl(0, 'SignifySignDelete', { link = 'diffRemoved' })
+      vim.api.nvim_set_hl(0, 'SignifySignDeleteFirstLine', { link = 'SignifySignDelete' })
+    end,
+  },
 }
