def9e41e082dd58c03635193d2412002cee5b163
Darkhan Kubigenov <darkhanu@gmail.com>
2023-01-20 00:55:14 +0000

    Unused files cleanup

--- dot_config/waybar/scripts/executable_get-default-pactl-sink.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pactl --format=json list sinks | jq -cM --unbuffered "map(select(.name == \"$(pactl get-default-sink)\"))[0].properties | {text:(.\"media.name\" // .\"alsa.name\" // .\"node.name\")}"
