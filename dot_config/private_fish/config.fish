8c3e30a81e229bd3ae0c3deee78e16c9f66d0f70
Darkhan Kubigenov <darkhanu@gmail.com>
2023-12-25 01:02:56 +0000

    Sync latest changes

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,4 +7,6 @@ if status is-interactive
 	set fish_cursor_insert line
 	set fish_cursor_replace_one underscore
 	set fish_cursor_visual block
+
+  alias v=nvim
 end
--- dot_config/wireplumber/bluetooth.lua.d/51-bluez-customization.lua
+++ dot_config/wireplumber/bluetooth.lua.d/51-bluez-customization.lua
@@ -1,1 +1,3 @@
 bluez_monitor.properties["bluez5.enable-hw-volume"] = false
+bluez_monitor.properties["bluez5.enable-sbc-xq"] = true
+bluez_monitor.properties["bluez5.bluez5.enable-msbc"] = true
