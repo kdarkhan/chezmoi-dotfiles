c0c79a18de3848adeb0f91a827751328a9bf1c1d
Darkhan Kubigenov <darkhanu@gmail.com>
2024-08-07 23:28:01 +0100

    Disable leader/ normal map

--- dot_config/nvim/lua/plugins/common.lua
+++ dot_config/nvim/lua/plugins/common.lua
@@ -93,8 +93,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
+      {
+        "<leader>/",
+        false
+      },
       {
         "<leader>fp",
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
