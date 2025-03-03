137e3bf01b80df6c4305fbd0a170966743ab4467
Darkhan Kubigenov <darkhanu@gmail.com>
2025-03-03 01:08:18 +0000

    Update nvim configs

--- dot_config/nvim/lua/plugins/common.lua
+++ dot_config/nvim/lua/plugins/common.lua
@@ -39,133 +39,6 @@ return {
     config = true,
   },
 
-  -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
-  },
-
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>/",
-        false,
-      },
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
-      {
-        "<leader>sg",
-        LazyVim.pick("live_grep", { root = false }),
-        desc = "Grep current dir",
-      },
-      {
-        "<leader>sG",
-        LazyVim.pick("live_grep"),
-        desc = "Grep root dir",
-      },
-      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (root)" },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-        mappings = {
-          i = {
-            ["<C-Y>"] = function(prompt_bufnr)
-              -- This helper copies the current line from the previewer below the cursor.
-              -- Handy when searching for imports of Java identifiers in other files.
-              local entry = require("telescope.actions.state").get_selected_entry()
-              local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-
-              local line = nil
-
-              if entry.lnum and picker.previewer then
-                local bufnr = picker.previewer.state.bufnr
-                line = vim.fn.getbufline(bufnr, entry.lnum)[1]
-              end
-
-              require("telescope.actions").close(prompt_bufnr)
-
-              -- if line and vim.api.nvim_buf_get_option(0, 'modifiable') then
-              --   vim.fn.append('.', line)
-              -- end
-              vim.fn.setreg('"', line, "l")
-            end,
-            -- ['<C-Down>'] = require('telescope.actions').cycle_history_next,
-            -- ['<C-Up>'] = require('telescope.actions').cycle_history_prev,
-          },
-        },
-      },
-    },
-  },
-
-  -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  -- { import = "lazyvim.plugins.extras.lang.typescript" },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
@@ -193,7 +66,7 @@ return {
   },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
   {
@@ -245,5 +118,21 @@ return {
       "nvim-telescope/telescope.nvim", -- optional
     },
     config = true,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        ghost_text = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
   },
 }
